98c91a89d97170155b945e6583f1ff089ad121a8
KennethCN <1901407185@qq.com>
2018-06-29 12:29:35 +0800

    secend interview

---
+++ InterviewAssignment/CustomStateMachineReporter.cs
@@ -0,0 +1,63 @@
+﻿using Appccelerate.StateMachine.Machine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewAssignment
+{
+
+    /// <summary>
+    /// Provides way to customize state reporting.
+    /// 
+    /// By default you cannot get container of states from Appccelerate state machine.
+    /// This class can be used for that. Also it has StateToString method to enable
+    /// printing hierarchical states.
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    /// <typeparam name="TEvent"></typeparam>
+    public class CustomStateMachineReporter<TState, TEvent> : IStateMachineReport<TState, TEvent>
+            where TState : IComparable
+    where TEvent : IComparable
+
+    {
+        IEnumerable<IState<TState, TEvent>> myStates;
+
+        public IEnumerable<IState<TState, TEvent>> States
+        {
+            get
+            {
+                return myStates;
+            }
+        }
+
+        public void Report(string name, IEnumerable<IState<TState, TEvent>> states, Initializable<TState> initialStateId)
+        {
+            myStates = states;
+        }
+
+        public string StateToString(TState state, string separator = ".")
+        {
+
+            //Your assignment is here!
+            //Tip: You find state machine hierarchy on States property (or myStates field). 
+            //You should go through the states and print that on what hierarchy path the current state
+            //is found. So if state is Initializing this method should return "Down.Initializing" because
+            //"Initializing" is substate of "Down".
+                       
+           //throw new NotImplementedException();
+
+            var allState = myStates.ToList();
+            var stat = allState.Where(s => s.Id.CompareTo(state) == 0);
+            var s0 = stat.ToArray()[0];
+            var rtn = s0.Id.ToString();
+            while (s0.SuperState != null)
+            {
+                s0 = s0.SuperState;
+                rtn = s0 + separator + rtn;
+            }
+            return rtn;
+                       
+
+        }
+    }
+}
